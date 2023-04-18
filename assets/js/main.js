8727b9456bcaa0b856c11a7d206d667ae93eddcb
alecasentini <ale.casentini@gmail.com>
2023-04-18 14:38:51 +0200

    Aggiunto css e flip card:hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ createApp ({
     data() {
         return {
             message:"Hello Vue!",
-            charizardUrl: './assets/img/004.jpg'
+            charizardUrl: './assets/img/004.jpg',
+            backCardUrl: './assets/img/back-card.jpg'
         }
     }
 }).mount ("#app")
